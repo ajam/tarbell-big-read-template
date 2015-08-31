17c38a3642de776d7048adf456f8d303b9c6515d
lamthuyvo <lamthuyvo@gmail.com>
2015-08-31 12:54:49 -0400

    added photo magnification function

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	// initializing image magnification library
+
+	$('.expand-image').magnificPopup({
+		type: 'image',
+		verticalFit: true,
+		closeOnContentClick: true,
+		closeBtnInside: true
+	});
+
+
+
+});
